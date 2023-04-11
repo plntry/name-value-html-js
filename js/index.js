360e01edd8367ec010cf5328f2dabe020acb717c
Oksana Nedilko <nedilko7b@gmail.com>
2023-04-11 17:08:07 +0300

    added name-value pair validation

--- js/index.js
+++ js/index.js
@@ -3,10 +3,29 @@ const nameValueInput = document.getElementById('name-value');
 const nameValueList = document.getElementById('name-value-list');
 
 function addNewNameValue() {
-    const nameValue = nameValueInput.value.trim();
+    const nameValue = nameValueInput.value;
+
+    // check if there is only one "=" character in the string
+    const equalSymbolRegex = /^[^=]*=[^=]*$/;
+    // check if name and value contain only alpha-numeric characters
+    const validStringRegex = /^[a-zA-ZА-Яа-яІіЇїЄєҐґ0-9]+$/;
+
+    const name = nameValue.split('=')[0].replace(/ /g, "");
+    const value = nameValue.split('=')[1].replace(/ /g, "");
+
     if (nameValue !== '') {
-        nameValueList.value += nameValue + '\n';
-        nameValueInput.value = '';
+        const hasEqualSymbol = nameValue.match(equalSymbolRegex);
+
+        if (hasEqualSymbol !== null) {
+            const isValidName = name.match(validStringRegex);
+
+            const isValidValue = value.match(validStringRegex);
+
+            if (isValidName !== null && isValidValue !== null) {
+                nameValueList.value += name + '=' + value + '\n';
+                nameValueInput.value = '';
+            }
+        }
     }
 }
 
